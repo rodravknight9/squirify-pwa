d3f283262bce56f4213a3e33e268b88d1c33dc2c
roderKnight <dealarcmarcel99@gmail.com>
2024-12-14 22:13:30 -0400

    Templates for the main page ready

--- src/Squirrify.jsx
+++ src/Squirrify.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import { AppRouter } from './router/AppRouter'
+import { AppRouter } from "./router/AppRouter";
 
 export const Squirrify = () => {
-    
-    return (
-        <>
-            <AppRouter />
-        </>
-    )
-}
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
+};
--- src/squirrify/pages/MainPage.jsx
+++ src/squirrify/pages/MainPage.jsx
@@ -1,32 +1,47 @@
-import React from 'react'
+import React from "react";
 
 export const MainPage = () => {
-    return (
-        <div className="container">
-
-            <div className="header">
-                <div className="total">
-                    <span className='number'>12738</span>
-                    <span className='currency'>Bs</span>
-                </div>
-
-                <div className="filter">
-                    <div className='options'>
-                        <span className='selected'>Today</span>
-                        <span>Month</span>
-                        <span>Year</span>
-                        <span>Custom</span>
-                    </div>
-                </div>
-            </div>        
-            
-            <div className='list-of-expenses'>
-
-            </div>
-        
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="total">
+          <span className="number">12738</span>
+          <span className="currency">Bs</span>
         </div>
-    )
-}
 
+        <div className="filter">
+          <div className="options">
+            <span className="selected">Today</span>
+            <span>Month</span>
+            <span>Year</span>
+            <span>Custom</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="list-of-expenses">
+        <div className="expense">
+          <span className="icon">🍔</span>
+          <p className="expense-title">Burger king</p>
+          <span className="expense-cost">32.67</span>
+        </div>
+        <div className="expense">
+          <span className="icon">🍔</span>
+          <p className="expense-title">Burger king</p>
+          <span className="expense-cost">32.67</span>
+        </div>
+        <div className="expense">
+          <span className="icon">🍔</span>
+          <p className="expense-title">Burger king</p>
+          <span className="expense-cost">32.67</span>
+        </div>
+      </div>
+
+      <div className="toolbar">
+        <button className="add-button">Add</button>
+      </div>
+    </div>
+  );
+};
 
 // https://www.freecodecamp.org/news/how-to-create-an-animated-hamburger-menu-in-react/
